512102100609c5e41c78669ac1cdb1df189ecd58
Keijiro Takahashi <keijiro@gmail.com>
2019-11-06 21:55:44 +0900

    Add drop-down for selectio InputAction

--- Assets/VFXPlayerInputBinder.cs
+++ Assets/VFXPlayerInputBinder.cs
@@ -5,32 +5,67 @@ using UnityEngine.InputSystem;
 
 namespace Pyro
 {
+    //
+    // A simple class that holds action name
+    // This class is just needed to present a action selection drop-down using
+    // a custom property drawer.
+    //
+    [System.Serializable]
+    public class PlayerInputActionReference { public string Name; }
+
+    //
+    // VFX Property binder with Player Input
+    //
     [VFXBinder("Input/Player Input")]
     public class VFXPlayerInputBinder : VFXBinderBase
     {
-        public string Property { get { return (string)m_Property; } set { m_Property = value; } }
+        #region Bound property
+
+        public string Property
+        {
+            get { return (string)_property; }
+            set { _property = value; }
+        }
 
         [VFXPropertyBinding("System.Single"), SerializeField]
-        protected ExposedProperty m_Property = "FloatProperty";
-        public PlayerInput PlayerInput;
-        public string ActionName = "fire";
+        protected ExposedProperty _property = "FloatProperty";
 
-        InputAction m_CachedAction = null;
+        #endregion
+
+        #region Bound PlayerInput and InputAction selection
+
+        public PlayerInput PlayerInput;
+        public PlayerInputActionReference Action;
+
+        #endregion
+
+        #region VFXBinder implementation
+
+        InputAction _cachedAction = null;
 
         public override bool IsValid(VisualEffect component)
         {
-            return PlayerInput?.actions.FindAction(ActionName) != null && component.HasFloat(m_Property);
+            // Check if the selection is valid.
+            return PlayerInput != null && Action != null &&
+                PlayerInput.actions.FindAction(Action.Name) != null &&
+                component.HasFloat(_property);
         }
 
         public override void UpdateBinding(VisualEffect component)
         {
-            m_CachedAction = m_CachedAction ?? PlayerInput?.actions.FindAction(ActionName);
-            component.SetFloat(m_Property, m_CachedAction.ReadValue<float>());
+            // Try caching the bound action.
+            if (_cachedAction == null && PlayerInput != null && Action != null)
+                _cachedAction = PlayerInput.actions.FindAction(Action.Name);
+
+            // Carry the value.
+            component.SetFloat(_property, _cachedAction.ReadValue<float>());
         }
 
         public override string ToString()
         {
-            return string.Format("Input Value: '{0}' -> {1}", m_Property, ActionName);
+            return string.Format("Input Value: '{0}' -> {1}", _property, Action?.Name);
         }
+
+        #endregion
     }
 }
